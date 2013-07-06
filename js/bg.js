960fa71518438d10a4eefecb7d78bb335f9fd603
Daniel Mills <mills.dma@gmail.com>
2013-07-06 01:05:24 -0400

    whitespace

--- js/bg.js
+++ js/bg.js
@@ -1,5 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-	chrome.tabs.create({'url':'follogin.html'},function(tab){
-
-	});
+	chrome.tabs.create({'url':'follogin.html'},function(tab){});
 });
